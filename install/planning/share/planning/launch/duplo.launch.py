de73cd0954855e5b43028be489a2fc34a508aab7
scott-w-schuster <scott.schuster1@icloud.com>
2026-05-07 13:44:52 -0700

    fixed launch file and added rviz config

--- install/planning/share/planning/launch/duplo.launch.py
+++ install/planning/share/planning/launch/duplo.launch.py
@@ -6,6 +6,7 @@ from launch.events import Shutdown
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import time
 import os
 
 def generate_launch_description():
@@ -17,33 +18,6 @@ def generate_launch_description():
 
     ur_type = LaunchConfiguration("ur_type", default="ur7e")
     launch_rviz = LaunchConfiguration("launch_rviz", default="false")
-
-    realsense_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('realsense2_camera'),
-                'launch',
-                'rs_launch.py'
-            )
-        ),
-        launch_arguments={
-            'pointcloud.enable': 'true',
-            'rgb_camera.color_profile': '1920x1080x30',
-        }.items(),
-    )
-
-    moveit_launch_file = os.path.join(
-        get_package_share_directory("ur_moveit_config"),
-        "launch",
-        "ur_moveit.launch.py"
-    )
-    moveit_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(moveit_launch_file),
-        launch_arguments={
-            "ur_type": ur_type,
-            "launch_rviz": launch_rviz
-        }.items(),
-    )
 
     ik_planner_node = Node(
         package='planning',
@@ -76,16 +50,11 @@ def generate_launch_description():
         output='screen',
     )
 
-    shutdown_on_any_exit = RegisterEventHandler(
-        OnProcessExit(
-            on_exit=[EmitEvent(event=Shutdown(reason='A launched process exited'))]
-        )
-    )
-
     return LaunchDescription([
-        realsense_launch,
-        moveit_launch,
-        ik_planner_node,
+        ExecuteProcess( cmd=[ 'ros2', 'launch', 'realsense2_camera', 'rs_launch.py', 'pointcloud.enable:=true', 'rgb_camera.color_profile:=1920x1080x30']),
+        ExecuteProcess( cmd=['ros2','run','ur7e_utils','enable_comms']),
+        ExecuteProcess(cmd=['ros2','run','ur7e_utils','tuck']),
+        ExecuteProcess(cmd=['ros2','launch','ur_moveit_config','ur_moveit.launch.py','ur_type:=ur7e','launch_rviz:=false']),
         camera_transform_node,
         planning_main_node,
         brick_detection_node,
@@ -94,5 +63,4 @@ def generate_launch_description():
             cmd=['rviz2', '-d', rviz_config],
             output='screen',
         ),
-        shutdown_on_any_exit,
     ])
