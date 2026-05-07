e0806ad20b6ec59919366e196f907a8db8bf5427
Joe Mattson <ijoeymatts@gmail.com>
2026-05-07 11:51:56 -0700

    launch file fix

--- src/planning/setup.py
+++ src/planning/setup.py
@@ -12,8 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py'))
-
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/planning', glob('launch/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
